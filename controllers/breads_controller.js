96706250bb47467699f4c7d2d1200d65a6f3a6fd
Chris Mendoza <cmend85@gmail.com>
2022-03-14 20:32:36 -0500

    Added edit routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,12 +16,19 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  console.log('REQ Object PARAMS =>', req.params )
   
   const currentIndex = req.params.arrayIndex
-
+  
   if (Bread[currentIndex]) {
     res.render('Show', {
       bread:Bread[currentIndex],
@@ -31,6 +38,7 @@ breads.get('/:arrayIndex', (req, res) => {
     res.render('notFound')
   }
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -51,5 +59,18 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
 module.exports = breads
